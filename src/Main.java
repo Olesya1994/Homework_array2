2c3ebedf86acc9e58787ded213badf08e6040ef9
Olesya1994 <olesia.liakhor@gmail.ru>
2023-02-22 16:26:55 +0300

    tasks1-4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
@@ -31,31 +32,33 @@ public class Main {
         int minSpending = arr[0];
         int maxSpending = arr[0];
         for (int i = 0; i < 30; i++) {
-            if (arr[i]<minSpending){
-                minSpending=arr[i];
+            if (arr[i] < minSpending) {
+                minSpending = arr[i];
             }
-            if (arr[i]>maxSpending){
-                maxSpending=arr[i];
+            if (arr[i] > maxSpending) {
+                maxSpending = arr[i];
             }
         }
         System.out.println("Минимальная сумма трат за день составила " + minSpending + " рублей");
         System.out.println("Максимальная сумма трат за день составила " + maxSpending + " рублей");
     }
+
     public static void task3() {
         int[] arr = generateRandomArray();
         int totalSpending = 0;
         for (int i = 0; i < 30; i++) {
             totalSpending += arr[i];
         }
-        double averageSpending = totalSpending/arr.length;
+        double averageSpending = totalSpending / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageSpending + " рублей");
     }
+
     public static void task4() {
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 0; i < reverseFullName.length/2; i++) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
             char a = reverseFullName[i];
-            reverseFullName[i]=reverseFullName[reverseFullName.length-1-i];
-            reverseFullName[reverseFullName.length-1-i]=a;
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = a;
         }
         System.out.println(Arrays.toString(reverseFullName));
     }
